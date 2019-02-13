d084dc7840ae7ec75a36201ea1529951b6c7c450
ndutagrace25 <ndutagrace25@gmail.com>
2019-02-13 11:45:08 +0300

    added the db

--- application/config/database.sample.php
+++ application/config/database.sample.php
@@ -75,10 +75,10 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'username' => 'root',
-	'password' => '',
-	'database' => 'ecommerce',
+	'hostname' => 'nanyukiappfactory-mysqldbserver.mysql.database.azure.com',
+	'username' => 'grace@nanyukiappfactory-mysqldbserver',
+	'password' => '@grace12',
+	'database' => 'garcepdb',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
